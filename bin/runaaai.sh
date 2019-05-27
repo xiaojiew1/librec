d72ce3e82d5495a9ea3bad3a3d3275c43ca9be39
xiaojiew1 <xiaojiew1@student.unimelb.edu.au>
2019-05-27 22:14:06 +1000

    x

--- bin/runaaai.sh
+++ bin/runaaai.sh
@@ -1,6 +1,6 @@
 for ubs_ratio in 0.01 0.05 0.1 0.5; do
-  dataset_name=coat-vd_${ubs_ratio}
+  dataset_name=coat_excl_${ubs_ratio}
   ./runall.sh ${dataset_name}
-  dataset_name=coat+vd_${ubs_ratio}
+  dataset_name=coat_incl_${ubs_ratio}
   ./runall.sh ${dataset_name}
 done
