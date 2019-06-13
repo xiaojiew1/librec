879c97bc9d100708ac14876bc26f952c8a9ce80b
xiaojiew1 <xiaojiew1@student.unimelb.edu.au>
2019-06-14 08:50:58 +1000

    x

--- bin/runaaai.sh
+++ bin/runaaai.sh
@@ -1,6 +1,15 @@
 for ubs_ratio in 0.01 0.05 0.1 0.5; do
+  dataset_name=music_excl_${ubs_ratio}
+  ./runall.sh ${dataset_name}
+  dataset_name=music_incl_${ubs_ratio}
+  ./runall.sh ${dataset_name}
+done
+exit
+
+for ubs_ratio in 0.01 0.05 0.1 0.5; do
   dataset_name=coat_excl_${ubs_ratio}
   ./runall.sh ${dataset_name}
   dataset_name=coat_incl_${ubs_ratio}
   ./runall.sh ${dataset_name}
 done
+exit
