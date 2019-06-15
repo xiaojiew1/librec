dd17cf6bc6bec56f1fe5240db5261bdd78ab0123
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2019-06-15 12:45:13 +1000

    x

--- bin/evalaaai.sh
+++ bin/evalaaai.sh
@@ -1,10 +1,9 @@
-result_dir=result
-rm -rf ${result_dir}
-mkdir ${result_dir}
+evalres_dir=evalres
+if [ ! -d ${evalres_dir} ]; then
+  mkdir ${evalres_dir}
+fi
 
-for ubs_ratio in 0.01 0.05 0.1 0.5; do
-  dataset_name=coat_excl_${ubs_ratio}
-  ./evalall.sh ${dataset_name} > result/${dataset_name}.csv
-  dataset_name=coat_incl_${ubs_ratio}
-  ./evalall.sh ${dataset_name} > result/${dataset_name}.csv
-done
+dataset_name=coat_excl_0.05
+./evalall.sh ${dataset_name} > ${evalres_dir}/${dataset_name}.csv
+dataset_name=coat_incl_0.05
+./evalall.sh ${dataset_name} > ${evalres_dir}/${dataset_name}.csv
