80d6a6caf208e4fe07258c130f2e62e085a10cc4
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2019-06-15 10:39:43 +1000

    x

--- bin/runaaai.sh
+++ bin/runaaai.sh
@@ -1,3 +1,9 @@
+dataset_name=music_excl_${ubs_ratio}
+./runall.sh ${dataset_name}
+dataset_name=music_incl_${ubs_ratio}
+./runall.sh ${dataset_name}
+exit
+
 dataset_name=coat_excl_0.05
 ./runall.sh ${dataset_name}
 dataset_name=coat_incl_0.05
@@ -16,11 +22,4 @@ dataset_name=movie_incl_0.05
 ./runall.sh ${dataset_name}
 exit
 
-for ubs_ratio in 0.01 0.05 0.1 0.5; do
-  dataset_name=music_excl_${ubs_ratio}
-  ./runall.sh ${dataset_name}
-  dataset_name=music_incl_${ubs_ratio}
-  ./runall.sh ${dataset_name}
-done
-exit
 
