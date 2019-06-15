a39f67ff5bcb54dc9a025f57dd7c1ee6becfec23
Xiaojie Wang <xiaojiew1@student.unimelb.edu.au>
2019-06-15 22:32:45 +1000

    x

--- bin/evalaaai.sh
+++ bin/evalaaai.sh
@@ -3,7 +3,14 @@ if [ ! -d ${evalres_dir} ]; then
   mkdir ${evalres_dir}
 fi
 
+dataset_name=music_excl_0.05
+./evalall.sh ${dataset_name} > ${evalres_dir}/${dataset_name}.csv
+dataset_name=music_incl_0.05
+./evalall.sh ${dataset_name} > ${evalres_dir}/${dataset_name}.csv
+exit
+
 dataset_name=coat_excl_0.05
 ./evalall.sh ${dataset_name} > ${evalres_dir}/${dataset_name}.csv
 dataset_name=coat_incl_0.05
 ./evalall.sh ${dataset_name} > ${evalres_dir}/${dataset_name}.csv
+exit
